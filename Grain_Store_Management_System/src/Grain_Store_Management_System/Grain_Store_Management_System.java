fcc643eea9d43d83d4a24caaeae3b700716d65f7
Thasuni Induma <thasuniinduma@gmail.com>
2024-11-21 08:18:08 +0530

    view completed

---
+++ Grain_Store_Management_System/src/Grain_Store_Management_System/Grain_Store_Management_System.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Grain_Store_Management_System;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Grain_Store_Management_System {
+    public static void main(String[] args) {
+        //Call the Homescreen Inteface
+        //new HomeScreen().setVisible(true);
+
+    }
+}
