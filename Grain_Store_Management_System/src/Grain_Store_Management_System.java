f995691da756190bc9f82881eea5ec1497123435
Dinuli-Disara <disara184@gmail.com>
2024-11-25 08:45:00 +0530

    added a comment

--- Grain_Store_Management_System/src/Grain_Store_Management_System.java
+++ Grain_Store_Management_System/src/Grain_Store_Management_System.java
@@ -1,6 +1,7 @@
 import Interfaces.HomeScreen;
 import Interfaces.DashboardView;
 
+//main class
 public class Grain_Store_Management_System {
     public static void main(String[] args) {
         //Call the Homescreen Inteface
