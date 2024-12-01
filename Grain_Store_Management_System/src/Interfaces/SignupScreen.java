b51511bc08c31fafdb25e2d05e964fbef5c3d458
Akila Pilapitiya <156910086+akilapilapitiya@users.noreply.github.com>
2024-12-01 19:37:55 +0530

    code changes

--- Grain_Store_Management_System/src/Interfaces/SignupScreen.java
+++ Grain_Store_Management_System/src/Interfaces/SignupScreen.java
@@ -3,19 +3,30 @@ package Interfaces;
 //Imports
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+
+import controller.userController;
+import db.DBConnection;
+import dto.userDto;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener; 
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
-
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.awt.*;
 
 public class SignupScreen extends JFrame{
     public SignupScreen(){
+        userController userController = new userController();
 
         //JFrame Definitions
         setTitle("Grain Store Managment System"); //Title Changed
@@ -32,7 +43,7 @@ public class SignupScreen extends JFrame{
         //JFrame Background Image
         ImageIcon backgroundImageSet = new ImageIcon(getClass().getResource("Assets/sideView.png"));
         Image imageSet = backgroundImageSet.getImage();
-        Image resizedImage = imageSet.getScaledInstance(1000, 700, Image.SCALE_SMOOTH);
+        Image resizedImage = imageSet.getScaledInstance(900, 600, Image.SCALE_SMOOTH);
         ImageIcon backgroundImage = new ImageIcon(resizedImage);
         JLabel backgroundImageSetter = new JLabel(backgroundImage);
         backgroundImageSetter.setBounds(0, 0, 1000, 700);
@@ -44,7 +55,7 @@ public class SignupScreen extends JFrame{
         Image resizedLogoImage = logoSet.getScaledInstance(400, 200, Image.SCALE_SMOOTH);
         ImageIcon logoImage = new ImageIcon(resizedLogoImage);
         JLabel logoImageSetter = new JLabel(logoImage);
-        logoImageSetter.setBounds(60, 50, 400, 200);
+        logoImageSetter.setBounds(60, 20, 400, 200);
 
         //JPanel for Context Box
         JPanel contentBox = new JPanel();
@@ -53,43 +64,57 @@ public class SignupScreen extends JFrame{
 
         //JLabel For Login Text
         JLabel SignUpText = new JLabel("Sign-up");
-        SignUpText.setBounds(170, 210, 150, 100);
+        SignUpText.setBounds(170, 140, 150, 100);
         SignUpText.setForeground(Color.black);
         SignUpText.setFont(new Font("Arial", Font.BOLD, 30));
 
         //Text Fields Defined for UserName
-        JTextField userNameTextBox = new JTextField("Enter your Name");
-        userNameTextBox.setBounds(140, 320, 180, 40);
+        JTextField userNameTextBox = new JTextField("Enter your User Name");
+        userNameTextBox.setBounds(140, 300, 180, 40);
         userNameTextBox.setFont(new Font("Arial", Font.ITALIC, 13));
 
         //Text Fields Defined for Password
         JTextField userMobileTextBox = new JTextField("Enter your Mobile Number");
-        userMobileTextBox.setBounds(140, 380, 180, 40);
+        userMobileTextBox.setBounds(140, 360, 180, 40);
         userMobileTextBox.setFont(new Font("Arial", Font.ITALIC, 13));
 
         //Text Fields Defined for Password
         JTextField userPasswordTextBox = new JTextField("Enter a Password");
-        userPasswordTextBox.setBounds(140, 440, 180, 40);
+        userPasswordTextBox.setBounds(140, 420, 180, 40);
         userPasswordTextBox.setFont(new Font("Arial", Font.ITALIC, 13));
 
         //Text Fields Defined for Confirm Password
         JTextField userConfirmPasswordTextBox = new JTextField("Confirm Password");
-        userConfirmPasswordTextBox.setBounds(140, 500, 180, 40);
+        userConfirmPasswordTextBox.setBounds(140, 480, 180, 40);
         userConfirmPasswordTextBox.setFont(new Font("Arial", Font.ITALIC, 13));
         
          //Buttons defined for Submit
         JButton submitButton = new JButton("Submit");
-        submitButton.setBounds(245, 560, 100, 40);
+        submitButton.setBounds(245, 590, 100, 40);
         submitButton.setBackground(new Color(166, 164, 164));
         submitButton.setForeground(Color.white);
         submitButton.setFont(new Font("Arial", Font.BOLD, 16));
 
         //Buttons defined for Back
         JButton backButton = new JButton("Back");
-        backButton.setBounds(120, 560, 100, 40);
+        backButton.setBounds(120, 590, 100, 40);
         backButton.setBackground(new Color(166, 164, 164));
         backButton.setForeground(Color.white);
         backButton.setFont(new Font("Arial", Font.BOLD, 16));
+
+        //Text Fields Defined for UserID
+        JTextField userIDTextBox = new JTextField("Enter your User ID");
+        userIDTextBox.setBounds(140, 240, 180, 40);
+        userIDTextBox.setFont(new Font("Arial", Font.ITALIC, 13));
+
+        // Create a dropdown list for Warehouse Type
+        
+        JComboBox<String> dropdown = new JComboBox<>();
+        dropdown.setBounds(140, 540, 180, 40);
+        dropdown.setBackground(Color.WHITE);
+        fetchWarehouseIDs(dropdown);
+        
+        
 
         // Add a FocusListener for userNameTextBox
         userNameTextBox.addFocusListener(new FocusListener() {
@@ -185,16 +210,18 @@ public class SignupScreen extends JFrame{
 
 
         //Add Elements to the Frame
-        add(logoImageSetter);
-        add(SignUpText);
-        add(userNameTextBox);
-        add(userMobileTextBox);
-        add(userPasswordTextBox);
-        add(userConfirmPasswordTextBox);
-        add(submitButton);
-        add(backButton);
-        add(contentBox);
-        add(backgroundImageSetter);
+                add(logoImageSetter);
+                add(SignUpText);
+                add(userIDTextBox);
+                add(userNameTextBox);
+                add(userMobileTextBox);
+                add(userPasswordTextBox);
+                add(userConfirmPasswordTextBox);
+                add(dropdown);
+                add(submitButton);
+                add(backButton);
+                add(contentBox);
+                add(backgroundImageSetter);
 
     }
     
